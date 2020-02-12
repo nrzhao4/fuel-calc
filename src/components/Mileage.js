ad36fad9638cec33ff46e44601a00354026472a7
Nina Zhao <nina.r.zhao@gmail.com>
2020-02-12 00:00:30 -0500

    Input distance and percent on highway, state gets updated

--- src/components/Mileage.js
+++ src/components/Mileage.js
@@ -1,12 +1,45 @@
 import React from 'react';
 
-function Mileage() {
-    return (
-        <div className='flexbox2'>
-            <label>Distance to drive: </label>
-            <label>Percentage of total distance spent on the highway: </label>
-        </div> 
-    )
+class Mileage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            distance: '',
+            units: '',
+            percentHwy: '',
+        }
+
+        this.handleDistance = this.handleDistance.bind(this);
+        this.handlePercentHwy = this.handlePercentHwy.bind(this);
+    }
+
+    handleDistance(event) {
+        this.setState({distance: event.target.value});
+    }
+
+    handlePercentHwy(event) {
+        this.setState({percentHwy: event.target.value});
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <div className='flexbox2'>
+                <div className='flexbox'>
+                    <label>Distance to drive: </label>
+                    <input type='text' value={this.state.distance} 
+                        onChange={this.handleDistance} />
+                </div>
+
+                <div className='flexbox'>
+                    <label>Percentage of total distance spent on the highway: </label>
+                    <input type='text' value={this.state.percentHwy} 
+                            onChange={this.handlePercentHwy} />
+                </div>
+                
+            </div>
+        )
+    }
 }
 
 export default Mileage;
