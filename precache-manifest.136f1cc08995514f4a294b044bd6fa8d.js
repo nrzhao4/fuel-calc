d5ce492fabd70dbbbfcc1b39bcc6fda39acf8e06
Nina Zhao <nina.r.zhao@gmail.com>
2020-02-23 20:40:23 -0500

    Updates

---
+++ precache-manifest.136f1cc08995514f4a294b044bd6fa8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8097d03f789a13a1d8e84f2bcd1e5c",
+    "url": "/fuel-calc/index.html"
+  },
+  {
+    "revision": "e8a730c9672958bf3eff",
+    "url": "/fuel-calc/static/css/main.394f1e13.chunk.css"
+  },
+  {
+    "revision": "b880855945eeaf6dc858",
+    "url": "/fuel-calc/static/js/2.e63fa1b8.chunk.js"
+  },
+  {
+    "revision": "bf91befff6f03511a9fca9de4a4d613f",
+    "url": "/fuel-calc/static/js/2.e63fa1b8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8a730c9672958bf3eff",
+    "url": "/fuel-calc/static/js/main.1fbf5784.chunk.js"
+  },
+  {
+    "revision": "332bbc8196b53db3f038",
+    "url": "/fuel-calc/static/js/runtime-main.b839f708.js"
+  }
+]);
